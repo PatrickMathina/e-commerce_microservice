99f4b9988880ba9992b830454e630a2bb847b3fc
Mathina Patrick <patrick.mathina@tangazoletu.com>
2025-04-24 15:38:08 +0300

    Initial commit

---
+++ src/test/java/com/patrickmath/onlineshop/OnlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrickmath.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
